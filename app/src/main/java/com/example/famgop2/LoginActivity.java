aa500e0a3ce66e373e1713b3ce43901b4d5033cd
EmmanuelRuiz5307 <emmaprofemx@gmail.com>
2022-04-21 22:51:25 -0500

    P3 Implementacion del LOGIN funcional

--- app/src/main/java/com/example/famgop2/LoginActivity.java
+++ app/src/main/java/com/example/famgop2/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.famgop2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +23,9 @@ public class LoginActivity extends AppCompatActivity {
     Button signIn;
     EditText email , password;
     TextView signUp;
+
+    //Creacion de objeto Firebase auth
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         email  = findViewById(R.id.email_login);
         password = findViewById(R.id.password_login);
         signUp = findViewById(R.id.sign_up);
-
+        auth = FirebaseAuth.getInstance();
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,10 +47,45 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //Uso del metodo loginUser()
+                loginUser();
             }
         });
 
 
     }
-}
+
+    private void loginUser() {
+        String user_email = email.getText().toString();
+        String user_pass = password.getText().toString();
+        if(TextUtils.isEmpty(user_email)){
+            Toast.makeText(this, "Email is Empty", Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        if(TextUtils.isEmpty(user_pass)){
+            Toast.makeText(this, "Password is Empty", Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        if(user_pass.length() < 6){
+            Toast.makeText(this, "Password will return 6 caracteres ", Toast.LENGTH_SHORT).show();
+            return ;
+        }
+        auth.signInWithEmailAndPassword(user_email , user_pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login Sucessfull", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(LoginActivity.this, "Error " + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }//Fin del loginUser
+
+
+
+
+}//Fin de la clase
